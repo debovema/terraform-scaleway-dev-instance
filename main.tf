17479275706fff8a218b097ef5074bee801f6cb4
Fabien Fassler <fabien.fassler@gmail.com>
2020-02-09 22:04:20 +0100

    Upgrade to terraform 0.12

--- main.tf
+++ main.tf
@@ -1,57 +1,73 @@
-data "scaleway_image" "image" {
-  count        = "${var.node_count > 0 ? 1 : 0}"
-
-  architecture = "${var.server_arch}"
-  name         = "${var.server_image}"
+provider "scaleway" {
+  #access_key      = "<SCALEWAY-ACCESS-KEY>"
+  #secret_key      = "<SCALEWAY-SECRET-KEY>"
+  #organization_id = "<SCALEWAY-ORGANIZATION-ID>"
+  zone            = "fr-par-1"
+  region          = "fr-par"
 }
 
-resource "scaleway_server" "node" {
-  count               = "${var.node_count}"
+data "scaleway_image" "image" {
+  count = var.node_count > 0 ? 1 : 0
 
-  name                = "${var.node_name}-${count.index}"
+  architecture = var.server_arch
+  name         = var.server_image
+}
 
-  image               = "${data.scaleway_image.image.id}"
-  type                = "${var.server_type}"
-  dynamic_ip_required = true
-  boot_type           = "local"
+resource "scaleway_instance_server" "node" {
+  count = var.node_count
+
+  name = "${var.node_name}-${count.index}"
+
+  image               = data.scaleway_image.image[0].id
+  type                = var.server_type
+  enable_dynamic_ip   = true
+  # boot_type           = "local"
 
   # initialization sequence
-  cloudinit           = "${data.template_file.userdata.rendered}"
+  cloud_init = data.template_file.userdata.rendered
+
+  connection {
+    host = element(scaleway_instance_server.node.*.public_ip, count.index)
+    user = var.username
+    private_key = file("~/.ssh/scaleway")
+  }
+
   provisioner "remote-exec" {
     inline = [
       "tail -f /var/log/cloud-init-output.log &",
-      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 10; done;"
+      "while [ ! -f /var/lib/cloud/instance/boot-finished ]; do sleep 10; done;",
     ]
   }
   provisioner "local-exec" {
     command = "sleep 80" # wait more than 1 minute for the instance to be rebooted
   }
-
 }
 
 data "template_file" "userdata" {
-  template = "${file("${path.module}/cloud-init-user-data")}"
+  template = file("${path.module}/cloud-init-user-data")
 
-  vars {
-    codename = "${var.docker_distrib_codename}"
-    distrib  = "${var.docker_distrib}"
-    user     = "${var.username}"
+  vars = {
+    codename = var.docker_distrib_codename
+    distrib  = var.docker_distrib
+    user     = var.username
   }
 }
 
 resource "null_resource" "node" {
-  count = "${var.node_count}"
+  count = var.node_count
 
   connection {
-    host = "${element(scaleway_server.node.*.public_ip, count.index)}"
-    user = "${var.username}"
+    host = element(scaleway_instance_server.node.*.public_ip, count.index)
+    user = var.username
+    private_key = file("~/.ssh/scaleway")
   }
 
   provisioner "remote-exec" {
     inline = [
       "mkdir -p ~/www",
       "echo 'It works' > ~/www/index.html",
-      "docker run --name http-nginx --restart=always -v ~/www:/usr/share/nginx/html:ro -p 80:80 -d nginx"
+      "docker run --name http-nginx --restart=always -v ~/www:/usr/share/nginx/html:ro -p 80:80 -d nginx",
     ]
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,39 +1,40 @@
 variable "node_count" {
-  type    = "string"
+  type    = string
   default = "1"
 }
 
 variable "node_name" {
-  type    = "string"
+  type    = string
   default = "cloud-init"
 }
 
 variable "server_arch" {
-  type    = "string"
+  type    = string
   default = "x86_64"
 }
 
 variable "server_image" {
-  type    = "string"
+  type    = string
   default = "Ubuntu Bionic"
 }
 
 variable "server_type" {
-  type    = "string"
+  type    = string
   default = "START1-S"
 }
 
 variable "username" {
-  type    = "string"
+  type    = string
   default = "user"
 }
 
 variable "docker_distrib" {
-  type    = "string"
+  type    = string
   default = "ubuntu"
 }
 
 variable "docker_distrib_codename" {
-  type    = "string"
+  type    = string
   default = "bionic"
 }
+
